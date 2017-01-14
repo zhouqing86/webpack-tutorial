5c5c1f9b6980efaee1632e05b2b38632fe06ab6d
Qing Zhou <qing.zhou@rea-group.com>
2017-01-14 10:26:32 +0800

    add environment production for build, development for dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,24 @@
 var path = require('path');
 var webpack = require('webpack');
 
-module.exports = {
-  entry: {
-    app: [
+var DEVELOPMENT = process.env.NODE_ENV === 'development';
+var PRODUCTION = process.env.NODE_ENV === 'production';
+
+var app = PRODUCTION
+  ? ["./src/index.js"]
+  : [
       "./src/index.js",
       "webpack-dev-server/client?http://localhost:8080/",
       "webpack/hot/dev-server"
-    ]
+    ];
+
+var plugins = PRODUCTION ? [] : [new webpack.HotModuleReplacementPlugin()];
+
+module.exports = {
+  entry: {
+    app: app
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
+  plugins: plugins,
   output: {
     path: path.resolve(__dirname, "build"),
     publicPath: "/assets/",
