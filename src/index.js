d8586dd353e436a370b70a0a8a2f8422441807fc
Qing Zhou <qing.zhou@rea-group.com>
2017-01-14 18:17:47 +0800

    import jquery will make 'npm run build' has a big bundle.js, will solve on the next commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ var app = document.getElementById('app');
 
 import Button from './button';
 import cat from './image';
+import $ from 'jquery';
 
 // var hiMessage = () => (`<p>${messages.hi},${messages.test} ${cat}</p>`);
 // var hiMessage = () => (`<div class=${style.box}><p>Dev:${DEVELOPMENT}<br/>Prod:${PRODUCTION}</p></div>${cat}`);
@@ -33,6 +34,8 @@ document.getElementById('loadpage2').addEventListener('click', () => {
     });
 });
 // Button.attachEl();
+
+$('#app').css('background-color', '#FF0000');
 
 if(DEVELOPMENT) {if(module.hot)
   console.log('DEVELOPMENT environment!'); //Because uglify, this won't appear in the built bundle.js
